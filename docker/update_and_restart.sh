3e4395bc1ca1d27280a2e60cdd02e83574836dd6
debian-go <438092+upbeat-backbone-bose@users.noreply.github.com>
2025-04-02 23:15:38 +0800

    更新 update_and_restart.sh
    add gh-proxy

--- docker/update_and_restart.sh
+++ docker/update_and_restart.sh
@@ -3,11 +3,11 @@ while true;
 do
     date
     echo "updating GeoLite2-City.mmdb..."
-    curl  -L -o "GeoLite2-City.mmdb" "https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-City.mmdb"
+    curl  -L -o "GeoLite2-City.mmdb" "https://gh-proxy.com/github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-City.mmdb"
     echo "updating GeoLite2-ASN.mmdb..."
-    curl  -L -o "GeoLite2-ASN.mmdb" "https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-ASN.mmdb"
+    curl  -L -o "GeoLite2-ASN.mmdb" "https://gh-proxy.com/github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-ASN.mmdb"
     echo "updating GeoCN.mmdb..."
-    curl  -L -o "GeoCN.mmdb" "http://github.com/ljxi/GeoCN/releases/download/Latest/GeoCN.mmdb"
+    curl  -L -o "GeoCN.mmdb" "https://gh-proxy.com/github.com/ljxi/GeoCN/releases/download/Latest/GeoCN.mmdb"
 
     echo "restarting uvicorn..."
     pkill -f "uvicorn"
